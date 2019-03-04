0cea8892ddfcee807e5aeee8ae6e26d27814201c
Alexa Heitzenrader <aheitzen1@gmail.com>
2019-03-04 14:01:36 -0800

    bodyMovin bg

--- js/main.js
+++ js/main.js
@@ -1,3 +1,21 @@
 particlesJS.load('particles-js', 'assets/particles.json', function() {
   console.log('callback - particles.js config loaded');
 });
+
+lottie.loadAnimation({
+  container: document.getElementById('circle-animation'), // the dom element that will contain the animation
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  path: 'assets/background-one.json' // the path to the animation json
+});
+
+lottie.loadAnimation({
+  container: document.getElementById('sm-circle-animation'), // the dom element that will contain the animation
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  path: 'assets/sm.json' // the path to the animation json
+});
+
+// anim.play();
