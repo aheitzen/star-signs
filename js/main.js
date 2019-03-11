29e4481bfe31aaaffffd045a4dbb9c0c6da79df4
Alexa Heitzenrader <aheitzen1@gmail.com>
2019-03-11 09:41:24 -0700

    added first iteration signs

--- js/main.js
+++ js/main.js
@@ -2,6 +2,25 @@ particlesJS.load('particles-js', 'assets/particlesjs-config.json', function() {
   console.log('callback - particles.js config loaded');
 });
 
+function load(url, element)
+{
+    if (!url || !element) {
+        return;
+    }
+
+    req = new XMLHttpRequest();
+    req.open("GET", url, false);
+    req.send(null);
+
+    element.innerHTML = req.responseText; 
+}
+
+load('./partials/aries.html', document.querySelector('#aries-page'));
+load('./partials/cancer.html', document.querySelector('#cancer-page'));
+load('./partials/libra.html', document.querySelector('#libra-page'));
+
+
+
 lottie.loadAnimation({
   container: document.getElementById('circle-animation'), // the dom element that will contain the animation
   renderer: 'svg',
@@ -10,5 +29,24 @@ lottie.loadAnimation({
   path: 'assets/aries-season.json' // the path to the animation json
 });
 
+lottie.loadAnimation({
+  container: document.getElementById('cancer-animation'), // the dom element that will contain the animation
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  path: 'assets/cancer.json' // the path to the animation json
+});
+
+lottie.loadAnimation({
+  container: document.getElementById('libra-animation'), // the dom element that will contain the animation
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  path: 'assets/libra-animation.json' // the path to the animation json
+});
+
 new WOW().init();
+// const mySiema = new Siema();
+// document.querySelector('.prev').addEventListener('click', () => mySiema.prev());
+// document.querySelector('.next').addEventListener('click', () => mySiema.next());
         
