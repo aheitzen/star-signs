84a18a710e37e6941afc7cdb4e70518eb135928e
Alexa Heitzenrader <aheitzen1@gmail.com>
2019-03-16 20:59:00 -0700

    clean code

--- js/main.js
+++ js/main.js
@@ -1,13 +1,10 @@
-particlesJS.load('particles-js', 'assets/particlesjs-config.json', function() {
-  console.log('callback - particles.js config loaded');
-});
+particlesJS.load('particles-js', 'assets/particlesjs-config.json', function() {});
 
 function load(url, element)
 {
-    if (!url || !element) {
-        return;
-    }
-
+  if (!url || !element) {
+    return;
+  }
     req = new XMLHttpRequest();
     req.open("GET", url, false);
     req.send(null);
@@ -28,7 +25,6 @@ load('./partials/taurus.html', document.querySelector('#taurus-page'));
 load('./partials/cap.html', document.querySelector('#cap-page'));
 load('./partials/sop.html', document.querySelector('#sop-page'));
 
-
 var initialIndex = 0
 var initialNextName = 'Taurus'
 var initialPrevName = 'Pisces'
@@ -44,142 +40,133 @@ if (initialIndex == 1) {
   initialPrevName = 'Taurus';
 }
 
-
 //ARIES ANIMATION
 var aries = lottie.loadAnimation({
-  container: document.getElementById('circle-animation'), // the dom element that will contain the animation
+  container: document.getElementById('circle-animation'), 
   renderer: 'svg',
   loop: true,
   autoplay: false,
-  path: 'assets/aries.json' // the path to the animation json
+  path: 'assets/aries.json' 
 });
 
 // CANCER ANIMATION
 var cancer = lottie.loadAnimation({
-  container: document.getElementById('cancer-animation'), // the dom element that will contain the animation
+  container: document.getElementById('cancer-animation'), 
   renderer: 'svg',
   loop: true,
   autoplay: false,
-  path: 'assets/cancer.json' // the path to the animation json
+  path: 'assets/cancer.json' 
 });
 
 // LIBRA ANIMATION DIS ONE
 var libra = lottie.loadAnimation({
-  container: document.getElementById('libra-animation'), // the dom element that will contain the animation
+  container: document.getElementById('libra-animation'), 
   renderer: 'svg',
   loop: true,
   autoplay: false,
-  path: 'assets/libra-animation.json' // the path to the animation json
+  path: 'assets/libra-animation.json' 
 });
 
 // PISCES ANIMATION
 var pisces = lottie.loadAnimation({
-  container: document.getElementById('pisces-animation'), // the dom element that will contain the animation
+  container: document.getElementById('pisces-animation'), 
   renderer: 'svg',
   loop: true,
   autoplay: false,
-  path: 'assets/pisces-animation.json' // the path to the animation json
+  path: 'assets/pisces-animation.json' 
 });
 
 // SAG ANIMATION
 var sag = lottie.loadAnimation({
-  container: document.getElementById('sag-animation'), // the dom element that will contain the animation
+  container: document.getElementById('sag-animation'), 
   renderer: 'svg',
   loop: true,
   autoplay: false,
-  path: 'assets/sag.json' // the path to the animation json
+  path: 'assets/sag.json' 
 });
 
 // AQUA ANIMATION
 var aqua = lottie.loadAnimation({
-  container: document.getElementById('aqua-animation'), // the dom element that will contain the animation
+  container: document.getElementById('aqua-animation'), 
   renderer: 'svg',
   loop: true,
   autoplay: false,
-  path: 'assets/aqua.json' // the path to the animation json
+  path: 'assets/aqua.json' 
 });
 
 // VIRGO ANIMATION
 var virgo = lottie.loadAnimation({
-  container: document.getElementById('virgo-animation'), // the dom element that will contain the animation
+  container: document.getElementById('virgo-animation'), 
   renderer: 'svg',
   loop: true,
   autoplay: false,
-  path: 'assets/virgo.json' // the path to the animation json
+  path: 'assets/virgo.json' 
 });
 
 // LEO ANIMATION
 var leo = lottie.loadAnimation({
-  container: document.getElementById('leo-animation'), // the dom element that will contain the animation
+  container: document.getElementById('leo-animation'), 
   renderer: 'svg',
   loop: true,
   autoplay: false,
-  path: 'assets/leo.json' // the path to the animation json
+  path: 'assets/leo.json' 
 });
 
 // GEMINI ANIMATION
 var gemini = lottie.loadAnimation({
-  container: document.getElementById('gemini-animation'), // the dom element that will contain the animation
+  container: document.getElementById('gemini-animation'), 
   renderer: 'svg',
   loop: true,
   autoplay: false,
-  path: 'assets/gemini.json' // the path to the animation json
+  path: 'assets/gemini.json' 
 });
 
 // TAURUS ANIMATION
 var taurus = lottie.loadAnimation({
-  container: document.getElementById('taurus-animation'), // the dom element that will contain the animation
+  container: document.getElementById('taurus-animation'), 
   renderer: 'svg',
   loop: true,
   autoplay: false,
-  path: 'assets/taurus.json' // the path to the animation json
+  path: 'assets/taurus.json' 
 });
 
 // CAP ANIMATION
 var cap = lottie.loadAnimation({
-  container: document.getElementById('cap-animation'), // the dom element that will contain the animation
+  container: document.getElementById('cap-animation'), 
   renderer: 'svg',
   loop: true,
   autoplay: false,
-  path: 'assets/capricorn.json' // the path to the animation json
+  path: 'assets/capricorn.json' 
 });
 
 //SOP ANIMATION
 var sop = lottie.loadAnimation({
-  container: document.getElementById('sop-animation'), // the dom element that will contain the animation
+  container: document.getElementById('sop-animation'), 
   renderer: 'svg',
   loop: true,
   autoplay: false,
-  path: 'assets/sop.json' // the path to the animation json
+  path: 'assets/sop.json' 
 });
-
-
-
 
 $('.main-container').on('afterChange', function(event, slick, currentSlide){
     var nextArrow = document.querySelector('#nextName');
     var prevArrow = document.querySelector('#prevName');
     if (currentSlide == 0) {
-      // ARIES ANIMATION
       aries.play();
       prevArrow.innerHTML = 'Pisces';
     }  else if (currentSlide == 1) {
-      //CANCER ANIMATION
       taurus.play();
       nextArrow.innerHTML = 'Gemini';
       prevArrow.innerHTML = 'Aries';
     } else if (currentSlide == 2) {
-      // LIBRA ANIMATION
       gemini.play();
       nextArrow.innerHTML = 'Cancer';
       prevArrow.innerHTML = 'Taurus';
     } else if (currentSlide == 3) {
-      //PISCES ANIMATION
       cancer.play();
       nextArrow.innerHTML = 'Leo';
       prevArrow.innerHTML = 'Gemini';
     } else if (currentSlide == 4) {
-      //SAG ANIMATION
       leo.play();
       nextArrow.innerHTML = 'Virgo';
       prevArrow.innerHTML = 'Cancer';
@@ -188,32 +175,26 @@ $('.main-container').on('afterChange', function(event, slick, currentSlide){
       nextArrow.innerHTML = 'Libra';
       prevArrow.innerHTML = 'Leo';
     } else if (currentSlide == 6) {
-      // VIRGO ANIMATION
       libra.play();
       nextArrow.innerHTML = 'Scorpio';
       prevArrow.innerHTML = 'Virgo';
     } else if (currentSlide == 7) {
-      //LEO ANIMATION
       sop.play();
       nextArrow.innerHTML = 'Sagittarius';
       prevArrow.innerHTML = 'Libra';
     } else if (currentSlide == 8) {
-      // GEMINI ANIMATION
       sag.play();
       nextArrow.innerHTML = 'Capricorn';
       prevArrow.innerHTML = 'Scorpio';
     } else if (currentSlide == 9) {
-      // TAURUS ANIMATION
       cap.play();
       nextArrow.innerHTML = 'Aquarius';
       prevArrow.innerHTML = 'Sagittarius';
     } else if (currentSlide == 10) {
-      // CAP ANIMAITON
       aqua.play();
       nextArrow.innerHTML = 'Pisces';
       prevArrow.innerHTML = 'Capricorn';
     } else if (currentSlide == 11) {
-      // SOP ANIMAITON
       pisces.play();
       nextArrow.innerHTML = 'Aries';
       prevArrow.innerHTML = 'Aquarius';
@@ -222,95 +203,63 @@ $('.main-container').on('afterChange', function(event, slick, currentSlide){
  })
 
 $('.main-container').on('beforeChange', function(event, slick, currentSlide, nextSlide){
-    // wow.stop();
-    // wow.resetAnimation();
     if (currentSlide == 0) {
-      // ARIES ANIMATION
       aries.stop();
     } else if (currentSlide == 1) {
-      //CANCER ANIMATION
       taurus.stop();
-
     } else if (currentSlide == 2) {
-      // LIBRA ANIMATION
       gemini.stop();
     } else if (currentSlide == 3) {
-      // LIBRA ANIMATION
       cancer.stop();
     } else if (currentSlide == 4) {
-      // SAG ANIMATION
       leo.stop();
     } else if (currentSlide == 5) {
-      // AQUA ANIMATION
       virgo.stop();
     } else if (currentSlide == 6) {
-      // VIRGO ANIMATION
       libra.stop();
     } else if (currentSlide == 7) {
-      //LEO ANIMATION
       sop.stop();
     } else if (currentSlide == 8) {
-      // GEMINI ANIMATION
       sag.stop();
     } else if (currentSlide == 9) {
-      // TAURUS ANIMATION
       cap.stop();
     } else if (currentSlide == 10) {
-      // CAP ANIMATION
       aqua.stop();
     } else if (currentSlide == 11) {
-      // CAP ANIMATION
       pisces.stop();
     }
 })
 
-// capricorn.json
-
 $('.main-container').on('init', function(event, slick){
-    // wow = new WOW().init();
-    // wow.start();
     var currentSlide =  initialIndex
     if (currentSlide == 0) {
-      // ARIES ANIMATION
       aries.play();
     }  else if (currentSlide == 1) {
-      //CANCER ANIMATION
       taurus.play();
     } else if (currentSlide == 2) {
-      // LIBRA ANIMATION
       gemini.play();
     } else if (currentSlide == 3) {
-      //PISCES ANIMATION
       cancer.play();
     } else if (currentSlide == 4) {
-      //SAG ANIMATION
       leo.play();
     } else if (currentSlide == 5) {
-      //AQUA ANIMATION
       virgo.play();
     } else if (currentSlide == 6) {
-      // VIRGO ANIMATION
       libra.play();
     } else if (currentSlide == 7) {
-      //LEO ANIMATION
       sop.play();
     } else if (currentSlide == 8) {
-      // GEMINI ANIMATION
       sag.play();
     } else if (currentSlide == 9) {
-      // TAURUS ANIMATION
       cap.play();
     } else if (currentSlide == 10) {
-      // CAP ANIMAITON
       aqua.play();
     } else if (currentSlide == 11) {
-      // CAP ANIMAITON
       pisces.play();
     } 
  })
 
 $('.main-container').slick({
-   // dots: true
   nextArrow: '<div id="next"><i class="fas fa-arrow-right a-right control-c next slick-next"></i><span id="nextName">' + initialNextName + '</span></div>',
   prevArrow: '<div id="prev"><i class="fas fa-arrow-left a-left control-c prev slick-prev"></i><span id="prevName">' + initialPrevName + '</span></div>',
   initialSlide: initialIndex
